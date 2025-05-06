4e4842463bae5948fa3b1b29811960b0522d0021
Kristiyan Kirilov <kirilov1905@gmail.com>
2025-05-06 15:47:13 +0300

    Import test, successfully working

--- JavaCourseProject/src/bg/tu_varna/sit/a2/f23621659/Application.java
+++ JavaCourseProject/src/bg/tu_varna/sit/a2/f23621659/Application.java
@@ -5,37 +5,55 @@ import bg.tu_varna.sit.a2.f23621659.models.TableFormatter;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args)
     {
-//        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        List<List<String>> persons = Arrays.asList(
-                Arrays.asList("ID", "Name", "Job"),
-                Arrays.asList("1", "Steve Johnson", "Driver"),
-                Arrays.asList("2", "Alex Ford", "Construction worker"),
-                Arrays.asList("3", "Peter Tompson", "F1 Racer")
-        );
-        List<String> formattedPersonsData =  TableFormatter.formatData(persons);
-        FileManager.writeInFile("persons.txt", formattedPersonsData);
+//        List<List<String>> jobs = Arrays.asList(
+//                Arrays.asList("ID", "Name"),
+//                Arrays.asList("1", "Driver", "Driver"),
+//                Arrays.asList("2", "Construction worker"),
+//                Arrays.asList("3", "F1 Racer"),
+//                Arrays.asList("4", "Cook Chef"),
+//                Arrays.asList("5", "Zookeeper")
+//        );
+//
+//        List<String> formattedJobsData =  TableFormatter.formatData(jobs);
+//        FileManager.writeInFile("jobs.txt", formattedJobsData);
 
-        List<List<String>> events = Arrays.asList(
-                Arrays.asList("ID", "Name", "City"),
-                Arrays.asList("1", "F1 Race", "Monaco"),
-                Arrays.asList("2", "Football match", "Paris"),
-                Arrays.asList("3", "Parade", "Rome")
-        );
+        String input;
+        while(!(input = scanner.nextLine()).equals("END")) {
+            String[] command = input.split(" ");
 
-        List<String> formattedEventsData =  TableFormatter.formatData(events);
-        FileManager.writeInFile("events.txt", formattedEventsData);
-
-        List<String> outputData = FileManager.readFile("persons.txt");
-        for(String data: outputData)
-        {
-            System.out.println(data);
+            switch(command[0]) {
+                case "import":
+                    String fileName = command[1];
+                    FileManager.importTable(fileName);
+                    break;
+                default:
+                    break;
+            }
         }
 
-        FileManager.importTable("departments.txt");
+//        List<List<String>> events = Arrays.asList(
+//                Arrays.asList("ID", "Name", "City"),
+//                Arrays.asList("1", "F1 Race", "Monaco"),
+//                Arrays.asList("2", "Football match", "Paris"),
+//                Arrays.asList("3", "Parade", "Rome")
+//        );
+//
+//        List<String> formattedEventsData =  TableFormatter.formatData(events);
+//        FileManager.writeInFile("events.txt", formattedEventsData);
+//
+//        List<String> outputData = FileManager.readFile("persons.txt");
+//        for(String data: outputData)
+//        {
+//            System.out.println(data);
+//        }
+//
+//        FileManager.importTable("departments.txt");
     }
 }
